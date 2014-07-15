2cd90733c3b665d24d9bbf3a9e529175e13a4cf3
Spencer Eldred <eldredspencer@gmail.com>
2014-07-15 09:23:21 -0600

    Spencer's solution

--- dogs.rb
+++ dogs.rb
@@ -1,37 +1,38 @@
 require_relative "testing_library"
 
-dogs = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
 
 def how_many_dogs(dogs)
-
+  dogs.length
 end
 
 def name_lengths(dogs)
-
+  dogs.map {|dog| dog.length }
 end
 
 def reverse_dog_names(dogs)
-
+  dogs.map {|dog| dog.reverse }
 end
 
 def first_three_dogs_with_each(dogs)
-
+  temp = []
+  dogs.each {|dog| temp << dog if dog[0] < 'P'}
+  temp
 end
 
 def first_three_dogs_without_each(dogs)
-
+  dogs[0..2]
 end
 
 def reverse_case_dog_names(dogs)
-
+  dogs.map {|dog| dog.swapcase }
 end
 
 def sum_of_all_dog_name_lengths(dogs)
-
+  dogs.inject(0) {|sum, dog| sum += dog.length}
 end
 
 def dogs_with_long_names(dogs)
-
+  dogs.map {|dog| dog.length > 4 }
 end
 
 puts "*"*80
@@ -39,12 +40,16 @@ puts "Make each method return the correct value"
 puts "The check method will run and tell you if the answer is correct"
 puts "*"*80
 
-check("how_many_dogs", how_many_dogs(dogs) == 6)
-check("name_lengths", name_lengths(dogs) == [4, 8, 4, 6, 4, 7])
-check("reverse_dog_names", reverse_dog_names(dogs) == ["odiF", "hgielraH", "ilaM", "eixirT", "wonS", "yrotciV"])
-check("first_three_dogs_with_each", first_three_dogs_with_each(dogs) == ["Fido", "Harleigh", "Mali"])
-check("first_three_dogs_without_each", first_three_dogs_without_each(dogs) == ["Fido", "Harleigh", "Mali"])
-check("reverse_case_dog_names", reverse_case_dog_names(dogs) == ["fIDO", "hARLEIGH", "mALI", "tRIXIE", "sNOW", "vICTORY"])
-check("sum_of_all_dog_name_lengths", sum_of_all_dog_name_lengths(dogs) == 33)
-check("dogs_with_long_names", dogs_with_long_names(dogs) == [false, true, false, true, false, true])
+awesome_dog_array = ["Fido", "Harleigh", "Mali", "Trixie", "Snow", "Victory"]
+small_dog_array = ["Fido", "Harleigh"]
+
+check("how_many_dogs", how_many_dogs(awesome_dog_array) == 6)
+check("how_many_dogs", how_many_dogs(small_dog_array) == 2)
+check("name_lengths", name_lengths(awesome_dog_array) == [4, 8, 4, 6, 4, 7])
+check("reverse_dog_names", reverse_dog_names(awesome_dog_array) == ["odiF", "hgielraH", "ilaM", "eixirT", "wonS", "yrotciV"])
+check("first_three_dogs_with_each", first_three_dogs_with_each(awesome_dog_array) == ["Fido", "Harleigh", "Mali"])
+check("first_three_dogs_without_each", first_three_dogs_without_each(awesome_dog_array) == ["Fido", "Harleigh", "Mali"])
+check("reverse_case_dog_names", reverse_case_dog_names(awesome_dog_array) == ["fIDO", "hARLEIGH", "mALI", "tRIXIE", "sNOW", "vICTORY"])
+check("sum_of_all_dog_name_lengths", sum_of_all_dog_name_lengths(awesome_dog_array) == 33)
+check("dogs_with_long_names", dogs_with_long_names(awesome_dog_array) == [false, true, false, true, false, true])
 
